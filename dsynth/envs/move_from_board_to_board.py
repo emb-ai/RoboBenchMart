5b63e7adc6c226f0be8baf8c7bedab2a9401af10
Alexander Krapukhin <alekrap@gmail.com>
2025-04-11 18:24:27 +0300

    Add tasks with demo teleop trajectories

--- dsynth/envs/move_from_board_to_board.py
+++ dsynth/envs/move_from_board_to_board.py
@@ -8,17 +8,17 @@ from mani_skill.utils.building import actors
 from mani_skill.utils.registration import register_env
 from dsynth.envs.darkstore_cell_base import DarkstoreCellBaseEnv
 from mani_skill.examples.motionplanning.panda.utils import get_actor_obb
-import copy
 
-LANGUAGE_INSTRUCTION = 'pick a milk from the shelf and put it on the cart'
-
-@register_env('PickToCartEnv', max_episode_steps=200000)
-class PickToCartEnv(DarkstoreCellBaseEnv):
+@register_env('MoveFromBoardToBoardEnv', max_episode_steps=200000)
+class MoveFromBoardToBoardEnv(DarkstoreCellBaseEnv):
 
     def _load_scene(self, options: dict):
         super()._load_scene(options)
 
-        self.target_sizes = np.array([0.3, 0.3, 0.3])
+        target_pose = self.actors['products'][self.target_product_name]['actor'].pose
+        target_pose.raw_pose[0,2] += get_actor_obb(self.actors['products'][self.target_product_name]['actor']).extents[2]/2 + 0.397 #height of board
+        target_pose.raw_pose[0][3:] = torch.Tensor([1, 0, 0, 0])
+        self.target_sizes = get_actor_obb(self.actors['products'][self.target_product_name]['actor']).extents
         self.target_volume = actors.build_box(
             self.scene,
             half_sizes=list(self.target_sizes/2),
@@ -26,7 +26,7 @@ class PickToCartEnv(DarkstoreCellBaseEnv):
             name="target_box",
             body_type="static",
             add_collision=False,
-            initial_pose=self.shopping_cart.pose,
+            initial_pose=target_pose,
         )
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
@@ -35,13 +35,13 @@ class PickToCartEnv(DarkstoreCellBaseEnv):
         if self.robot_uids == "panda_wristcam":
             qpos = np.array(
                 [
-                    -0.006,        
-                    -1.467,
-                    0.012,        
-                    -2.823,
-                    0.003,        
-                    2.928,
-                    0.796,
+                    0.0,        
+                    -np.pi / 6, 
+                    0.0,        
+                    -np.pi / 3, 
+                    0.0,        
+                    np.pi / 2,  
+                    np.pi / 4,  
                     0.04,       
                     0.04,       
                 ]
@@ -51,12 +51,8 @@ class PickToCartEnv(DarkstoreCellBaseEnv):
 
         robot_pose = self.agent.robot.get_pose()
         cart_shift = np.array([0.4, -0.2, 0.])
-        cube_shift_up = np.array([0, 0.13, 0.35])
         new_cart_pose_p = robot_pose.p[0].cpu().numpy() + cart_shift 
         
         self.shopping_cart.set_pose(sapien.Pose(p=new_cart_pose_p, q=robot_pose.q[0].numpy()))
-        self.target_volume.set_pose(sapien.Pose(p=new_cart_pose_p + cube_shift_up, q=robot_pose.q[0].numpy()))
         self.agent.robot.set_pose(sapien.Pose([0.5, 1.7, 0.0], [0.70710678118, 0, 0, 0.70710678118]))
         self.setup_target_object()
-
-
