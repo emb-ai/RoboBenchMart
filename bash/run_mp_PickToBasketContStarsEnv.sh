227a3b0ce91d152983892d3e2fc1dc4d1bf14092
Konstantin Soshin <soshin@airi.net>
2025-07-20 16:29:09 +0300

    Add bash scripts for MP

---
+++ bash/run_mp_PickToBasketContStarsEnv.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+DATA_PATH=/home/jovyan/shares/SR006.nfs2/data/dsynth
+
+python scripts/run_mp.py -e PickToBasketContStarsEnv --scene-dir \
+$DATA_PATH/demo_envs/pick_to_basket --only-count-success --num-procs 4 --num-traj 250 \
+--traj-name pick_to_basket_stars_250traj_4workers  
+
+
