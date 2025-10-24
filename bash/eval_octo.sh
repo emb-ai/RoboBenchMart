4a1871f8d34aaf7912fd7abadbebe08e72a933bf
Konstantin Soshin <soshin@airi.net>
2025-10-24 17:33:46 +0300

    Updated eval octo and generate_scenes

--- bash/eval_octo.sh
+++ bash/eval_octo.sh
@@ -2,6 +2,8 @@
 
 NET_PARAMS="--host=localhost --port=8000"
 EVAL_PARAMS="--max-horizon 500 --num-traj 30 --save-video"
+# ENV_PARAMS="MS_ASSET_DIR=/mnt/disk2tb/maniskill/"
+ENV_PARAMS=""
 
 # =========================================================
 # move_from_board_to_board
@@ -9,19 +11,19 @@ EVAL_PARAMS="--max-horizon 500 --num-traj 30 --save-video"
 
 ## item: duff
 ### train seeds
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_duff \
 --json-path demo_envs/move_from_board_to_board_duff/demos/motionplanning/move_from_board_to_board_duff_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_duff \
 --json-path demo_envs/move_from_board_to_board_duff/demos/motionplanning/move_from_board_to_board_duff_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e MoveFromBoardToBoardDuffContEnv \
 --scene-dir demo_envs/test_unseen_scenes_move_from_board_to_board_duff \
 --eval-subdir octo_jax_1000K_unseen_scenes $EVAL_PARAMS
@@ -30,19 +32,19 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 
 ## item: nestle
 ### train seeds
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_nestle \
 --json-path demo_envs/move_from_board_to_board_nestle/demos/motionplanning/move_from_board_to_board_nestle_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_nestle \
 --json-path demo_envs/move_from_board_to_board_nestle/demos/motionplanning/move_from_board_to_board_nestle_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e MoveFromBoardToBoardNestleContEnv \
 --scene-dir demo_envs/test_unseen_scenes_move_from_board_to_board_nestle \
 --eval-subdir octo_jax_1000K_unseen_scenes $EVAL_PARAMS
@@ -50,19 +52,19 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 
 ## item: vanish
 ### train seeds
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_vanish \
 --json-path demo_envs/move_from_board_to_board_vanish/demos/motionplanning/move_from_board_to_board_vanish_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/move_from_board_to_board_vanish \
 --json-path demo_envs/move_from_board_to_board_vanish/demos/motionplanning/move_from_board_to_board_vanish_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e MoveFromBoardToBoardVanishContEnv \
 --scene-dir demo_envs/test_unseen_scenes_move_from_board_to_board_vanish \
 --eval-subdir octo_jax_1000K_unseen_scenes $EVAL_PARAMS
@@ -90,19 +92,19 @@ python scripts/eval_policy_client.py $NET_PARAMS \
 
 ## item: nivea
 ### train seeds
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_nivea_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train_nivea $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_nivea_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose_nivea $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e PickToBasketContNiveaEnv \
 --scene-dir demo_envs/test_unseen_items_pick_to_basket \
 --eval-subdir octo_jax_1000K_unseen_scenes_nivea $EVAL_PARAMS
@@ -110,19 +112,19 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 
 ## item: fanta
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_fanta_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train_fanta $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_fanta_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose_fanta $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e PickToBasketContFantaEnv \
 --scene-dir demo_envs/test_unseen_items_pick_to_basket \
 --eval-subdir octo_jax_1000K_unseen_scenes_fanta $EVAL_PARAMS
@@ -131,19 +133,19 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 
 ## item: stars
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_stars_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train_stars $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_to_basket \
 --json-path demo_envs/pick_to_basket/demos/motionplanning/pick_to_basket_stars_250traj_4workers.rgbd.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose_stars $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e PickToBasketContStarsEnv \
 --scene-dir demo_envs/test_unseen_items_pick_to_basket \
 --eval-subdir octo_jax_1000K_unseen_scenes_stars $EVAL_PARAMS
@@ -170,38 +172,38 @@ python scripts/eval_policy_client.py $NET_PARAMS \
 
 ## item: beans
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_from_floor/ \
 --json-path demo_envs/pick_from_floor/demos/motionplanning/pick_from_floor_beans_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train_beans $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_from_floor/ \
 --json-path demo_envs/pick_from_floor/demos/motionplanning/pick_from_floor_beans_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose_beans $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e PickFromFloorBeansContEnv \
 --scene-dir demo_envs/test_unseen_scenes_pick_from_floor \
 --eval-subdir octo_jax_1000K_unseen_scenes_beans $EVAL_PARAMS
 
 ## item: slam
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_from_floor/ \
 --json-path demo_envs/pick_from_floor/demos/motionplanning/pick_from_floor_slam_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --eval-subdir octo_jax_1000K_train_slam $EVAL_PARAMS
 
 ### train seeds, but randomize robot init pose
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 --scene-dir demo_envs/pick_from_floor/ \
 --json-path demo_envs/pick_from_floor/demos/motionplanning/pick_from_floor_slam_250traj_4workers.rgb.pd_joint_pos.physx_cpu.json \
 --robot-init-pose-start-seed 1000 --eval-subdir octo_jax_1000K_rand_init_pose_slam $EVAL_PARAMS
 
 ### unseen scenes
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e PickFromFloorSlamContEnv \
 --scene-dir demo_envs/test_unseen_scenes_pick_from_floor \
 --eval-subdir octo_jax_1000K_unseen_scenes_slam $EVAL_PARAMS
@@ -226,7 +228,7 @@ python scripts/eval_policy_client.py $NET_PARAMS \
 # open fridge
 # =========================================================
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e OpenDoorFridgeContEnv \
 --scene-dir demo_envs/open_fridge/ \
 --eval-subdir octo_jax_1000K $EVAL_PARAMS
@@ -235,7 +237,7 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 # open showcase
 # =========================================================
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e OpenDoorShowcaseContEnv \
 --scene-dir demo_envs/open_showcase/ \
 --eval-subdir octo_jax_1000K $EVAL_PARAMS
@@ -243,7 +245,7 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 # =========================================================
 # close fridge
 # =========================================================
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e CloseDoorFridgeContEnv \
 --scene-dir demo_envs/close_fridge/ \
 --eval-subdir octo_jax_1000K $EVAL_PARAMS
@@ -251,7 +253,7 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_P
 # =========================================================
 # close showcase
 # =========================================================
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_client.py $NET_PARAMS \
 -e CloseDoorShowcaseContEnv \
 --scene-dir demo_envs/close_showcase// \
 --eval-subdir octo_jax_1000K $EVAL_PARAMS
