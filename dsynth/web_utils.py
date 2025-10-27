31939cd04ec0d8eafe3a007a3d24435e0ef72da7
Konstantin Soshin <soshin@airi.net>
2025-10-28 01:49:49 +0300

    Updated web utils and octo eval

--- dsynth/web_utils.py
+++ dsynth/web_utils.py
@@ -7,13 +7,19 @@ import http
 import logging
 import time
 import traceback
+from typing import Dict, Optional, Tuple
 
+from typing_extensions import override
 import websockets.asyncio.server as _server
 import websockets.frames
+import websockets.sync.client
 import functools
 
 import msgpack
 import numpy as np
+
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -133,3 +139,51 @@ def _health_check(connection: _server.ServerConnection, request: _server.Request
         return connection.respond(http.HTTPStatus.OK, "OK\n")
     # Continue with the normal request handling.
     return None
+
+class WebsocketClient:
+    """Implements the Policy interface by communicating with a server over websocket.
+
+    Copy of https://github.com/Physical-Intelligence/openpi/blob/main/packages/openpi-client/src/openpi_client/websocket_client_policy.py 
+    """
+
+    def __init__(self, host: str = "0.0.0.0", port: Optional[int] = None, api_key: Optional[str] = None) -> None:
+        self._uri = f"ws://{host}"
+        if port is not None:
+            self._uri += f":{port}"
+        self._packer = Packer()
+        self._api_key = api_key
+        self._ws, self._server_metadata = self._wait_for_server()
+
+    def get_server_metadata(self) -> Dict:
+        return self._server_metadata
+
+    def _wait_for_server(self) -> Tuple[websockets.sync.client.ClientConnection, Dict]:
+        logging.info(f"Waiting for server at {self._uri}...")
+        while True:
+            try:
+                headers = {"Authorization": f"Api-Key {self._api_key}"} if self._api_key else None
+                conn = websockets.sync.client.connect(
+                    self._uri, compression=None, max_size=None, additional_headers=headers,
+                    ping_interval=300, ping_timeout=300
+                )
+                metadata = unpackb(conn.recv())
+                return conn, metadata
+            except ConnectionRefusedError:
+                logging.info("Still waiting for server...")
+                time.sleep(5)
+
+    @override
+    def infer(self, obs: Dict) -> Dict:  # noqa: UP006
+        data = self._packer.pack(obs)
+        self._ws.send(data)
+        # async with asyncio.timeout(timeout=10):
+        response = self._ws.recv()
+        # response = await self._ws.recv()
+        if isinstance(response, str):
+            # we're expecting bytes; if the server sends a string, it's an error.
+            raise RuntimeError(f"Error in inference server:\n{response}")
+        return unpackb(response)
+
+    @override
+    def reset(self) -> None:
+        pass
