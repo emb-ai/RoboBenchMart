c0e89e1b36c99814af66e054dfa9b90a57de2b66
Konstantin Soshin <soshin@airi.net>
2025-10-30 04:29:48 +0300

    Updated script

--- bash/eval_octo_composite_tasks.sh
+++ bash/eval_octo_composite_tasks.sh
@@ -2,12 +2,14 @@
 
 NET_PARAMS="--host=localhost --port=8000"
 EVAL_PARAMS="--max-horizon 1000 --num-traj 30 --save-video"
+# ENV_PARAMS="MS_ASSET_DIR=/mnt/disk2tb/maniskill/"
+ENV_PARAMS=""
 
 # =========================================================
 # pick to basket 2 items
 # =========================================================
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_composite_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_composite_client.py $NET_PARAMS \
 --env-id PickNiveaFantaEnv \
 --scene-dir demo_envs/composite_pick_to_basket --eval-subdir octo_jax_1000K_nivea_fanta $EVAL_PARAMS
 
@@ -15,7 +17,7 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_composite_client
 # pick to basket 3 items
 # =========================================================
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_composite_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_composite_client.py $NET_PARAMS \
 --env-id PickNiveaFantaStarsEnv \
 --scene-dir demo_envs/composite_pick_to_basket --eval-subdir octo_jax_1000K_nivea_fanta_stars $EVAL_PARAMS
 
@@ -23,7 +25,7 @@ MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_composite_client
 # open showcae, pick item, close showcase
 # =========================================================
 
-MS_ASSET_DIR=/mnt/disk2tb/maniskill/ python scripts/eval_policy_composite_client.py $NET_PARAMS \
+$ENV_PARAMS python scripts/eval_policy_composite_client.py $NET_PARAMS \
 --env-id OpenPickDuffCloseEnv \
 --scene-dir demo_envs/composite_pick_from_showcase --eval-subdir octo_jax_1000K_pick_from_showcase $EVAL_PARAMS
 
